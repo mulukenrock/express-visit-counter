23f7e59e5c9fbdb7771f5634291e783bf44a548c
muluken <mulukenrock@gmail.com>
2022-05-19 18:22:44 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# express-portfolio-visit-counter-api
+
+## Sample `.env` file
+
+```
+HASURA_GRAPHQL_URL=http://localhost:4040/v1/graphql
+HASURA_GRAPHQL_ADMIN_SECRET=secret
+EXPRESS_PORT=4041
+CORS_ALLOWED_SITE=http://localhost:8081
+```
+## Build Setup
+
+```bash
+# start express
+$ yarn dev
+
+```
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const body_parser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+
+const port = process.env.EXPRESS_PORT;
+
+var corsOptions = {
+  origin: process.env.CORS_ALLOWED_SITE,
+  optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
+app.use(body_parser.json({ limit: "100mb" }));
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(morgan("combined"));
+app.use(helmet());
+
+app.post("/count", require("./handlers/count"));
+app.get("/visits", require("./handlers/visits"));
+
+app.listen(port, () => {
+  console.log(`Express server listening on ${port}`);
+});
